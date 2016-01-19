1827f89f18211cc7f9d49130fca5bbf9a4fa3b54
biickvtkd <89579416@qq.com>
2016-01-20 02:07:37 +0800

    add a little activiey.xml

--- app/src/main/java/com/masterfan/cloudbook/activity/personal/PersonalDataActivity.java
+++ app/src/main/java/com/masterfan/cloudbook/activity/personal/PersonalDataActivity.java
@@ -10,10 +10,10 @@ import com.masterfan.library.ui.annotation.MTFActivityFeature;
  * 个人资料
  * Created by Administrator on 2016/1/20 0020.
  */
-@MTFActivityFeature(layout = R.layout.activity_personal_data,toolbar = R.id.toolbar)
+@MTFActivityFeature(layout = R.layout.activity_personal_data)
 public class PersonalDataActivity extends MTFBaseActivity {
     @Override
     public void initialize(Bundle savedInstanceState) {
-        toolbar.setTitle("个人资料");
+
     }
 }
--- app/src/main/java/com/masterfan/cloudbook/fragment/home/HomePersonalFragment.java
+++ app/src/main/java/com/masterfan/cloudbook/fragment/home/HomePersonalFragment.java
@@ -1,17 +1,37 @@
 package com.masterfan.cloudbook.fragment.home;
 
+import android.content.Intent;
+import android.util.Log;
+import android.widget.ImageView;
+
 import com.masterfan.cloudbook.R;
+import com.masterfan.cloudbook.activity.personal.PersonalDataActivity;
 import com.masterfan.library.ui.MTFBaseFragment;
 import com.masterfan.library.ui.annotation.MTFFragmentFeature;
+
+import org.xutils.view.annotation.ViewInject;
+
+import butterknife.OnClick;
 
 /**
  * Created by 13510 on 2016/1/19.
  */
-@MTFFragmentFeature(layout = R.layout.fragment_home_personal)
+@MTFFragmentFeature(layout = R.layout.activity_personal)
 public class HomePersonalFragment extends MTFBaseFragment {
-
+    @ViewInject(R.id.personal_head_img)
+    ImageView headImageView;
     @Override
     public void initialize() {
+
+    }
+    @OnClick(R.id.personal_head_img)
+    public void onclickHeadImage(){
+        Intent intent = new Intent(HomePersonalFragment.this.getContext(),PersonalDataActivity.class);
+        if(intent != null){
+            animStart(intent);
+        }else{
+            Log.i("AAAA","intent为空");
+        }
 
     }
 }
