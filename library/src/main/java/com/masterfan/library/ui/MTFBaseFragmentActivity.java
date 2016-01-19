8c47f7b81833dd9c0dd2fdd57c4f15da3a2805d1
fgq <343887511@qq.com>
2016-01-19 15:45:18 +0800

    add main layout

---
+++ library/src/main/java/com/masterfan/library/ui/MTFBaseFragmentActivity.java
@@ -0,0 +1,105 @@
+package com.masterfan.library.ui;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.masterfan.library.R;
+import com.masterfan.library.ui.annotation.MTFActivityFeature;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by 13510 on 2016/1/19.
+ */
+public abstract class MTFBaseFragmentActivity extends FragmentActivity {
+    public Activity context;
+    public Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.context = this;
+        MTFActivityFeature activityFeature = MTFBaseFragmentActivity.this.getClass().getAnnotation(MTFActivityFeature.class);
+        if(activityFeature == null) throw new IllegalArgumentException(">>> not set layout resources!");
+        if(activityFeature.status_bar_color() > 0 ) setStatusBarColor(activityFeature.status_bar_color());
+        setContentView(activityFeature.layout());
+        ButterKnife.bind(this);
+        if(activityFeature.toolbar() > 0) {
+            toolbar = (Toolbar) findViewById(activityFeature.toolbar());
+        }
+        initialize(savedInstanceState);
+    }
+
+    /**
+     * set status bar color
+     */
+    public void setStatusBarColor(int statusBarColor){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Do something for lollipop and above versions
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(context, statusBarColor));
+        }
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(statusBarColor);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    /**
+     * Initialization operations, such as fetch data and more....
+     */
+    public abstract void initialize(Bundle savedInstanceState);
+
+    /**
+     * anim launch Activity
+     * @param clazz class
+     */
+    public void animStart(Class clazz){
+        Intent intent = new Intent(context, clazz);
+        startActivity(intent);
+        overridePendingTransition(R.anim.roll_up, R.anim.roll);
+    }
+
+    /**
+     * anim launch Activity
+     * @param intent intent
+     */
+    public void animStart(Intent intent){
+        startActivity(intent);
+        overridePendingTransition(R.anim.roll_up, R.anim.roll);
+    }
+
+
+    /**
+     * anim finish Activity
+     */
+    public void animFinish(){
+        finish();
+        overridePendingTransition(0, R.anim.roll_down);
+    }
+}
