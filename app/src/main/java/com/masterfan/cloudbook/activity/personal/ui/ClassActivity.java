3f078c4d193913935cd21d570302ff04c284c93f
biickvtkd <89579416@qq.com>
2016-01-25 22:09:10 +0800

    修改了ListView,新增了一些页面

---
+++ app/src/main/java/com/masterfan/cloudbook/activity/personal/ui/ClassActivity.java
@@ -0,0 +1,21 @@
+package com.masterfan.cloudbook.activity.personal.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.masterfan.cloudbook.R;
+import com.masterfan.library.ui.MTFBaseActivity;
+import com.masterfan.library.ui.annotation.MTFActivityFeature;
+
+@MTFActivityFeature(layout = R.layout.activity_class)
+public class ClassActivity extends MTFBaseActivity {
+    @Override
+    public void initialize(Bundle savedInstanceState) {
+
+    }
+
+}
