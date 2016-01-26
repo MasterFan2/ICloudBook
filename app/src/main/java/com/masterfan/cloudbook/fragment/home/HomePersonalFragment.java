d0b9669126dbc90bbbf990c01ca06241bd9500aa
biickvtkd <89579416@qq.com>
2016-01-26 23:24:59 +0800

    增加了跳转

--- app/src/main/java/com/masterfan/cloudbook/fragment/home/HomePersonalFragment.java
+++ app/src/main/java/com/masterfan/cloudbook/fragment/home/HomePersonalFragment.java
@@ -7,12 +7,13 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.masterfan.cloudbook.R;
-import com.masterfan.cloudbook.activity.personal.BookFriendFragmentActivity;
-import com.masterfan.cloudbook.activity.personal.CommentActivity;
-import com.masterfan.cloudbook.activity.personal.MyUploadActivity;
-import com.masterfan.cloudbook.activity.personal.NoteBookActivity;
-import com.masterfan.cloudbook.activity.personal.PersonalDataActivity;
-import com.masterfan.cloudbook.activity.personal.WorksmanagementFragmentActivity;
+import com.masterfan.cloudbook.activity.personal.ui.BookFriendFragmentActivity;
+import com.masterfan.cloudbook.activity.personal.ui.CommentListActivity;
+import com.masterfan.cloudbook.activity.personal.ui.MessageListActivity;
+import com.masterfan.cloudbook.activity.personal.ui.MyUploadListActivity;
+import com.masterfan.cloudbook.activity.personal.ui.NoteBookListActivity;
+import com.masterfan.cloudbook.activity.personal.ui.PersonalDataActivity;
+import com.masterfan.cloudbook.activity.personal.ui.WorksmanagementFragmentActivity;
 import com.masterfan.library.ui.MTFBaseFragment;
 import com.masterfan.library.ui.annotation.MTFFragmentFeature;
 
@@ -35,13 +36,16 @@ public class HomePersonalFragment extends MTFBaseFragment {
     LinearLayout commentLayout;//我的评论
 
     @Bind(R.id.personal_notebook_layout)
-    LinearLayout noteBookLayout;//我的评论
+    LinearLayout noteBookLayout;//我的笔记
 
     @Bind(R.id.personal_upload_book_layout)
     LinearLayout uoloadBookLayout;//我的上传
 
     @Bind(R.id.personal_works_management_layout)
     LinearLayout workManagementLayout;//图书管理
+
+    @Bind(R.id.personal_message_center_layout)
+    LinearLayout messageLayout;//消息中心
 
     @Override
     public void initialize() {
@@ -70,7 +74,7 @@ public class HomePersonalFragment extends MTFBaseFragment {
 
     @OnClick(R.id.personal_comment_layout)
     public void onclickComment(View view){
-        Intent intent = new Intent(getActivity(),CommentActivity.class);
+        Intent intent = new Intent(getActivity(),CommentListActivity.class);
         if(intent != null){
             animStart(intent);
         }else{
@@ -80,7 +84,7 @@ public class HomePersonalFragment extends MTFBaseFragment {
 
     @OnClick(R.id.personal_notebook_layout)
     public void onclickNoteBook(View view){
-        Intent intent = new Intent(getActivity(),NoteBookActivity.class);
+        Intent intent = new Intent(getActivity(),NoteBookListActivity.class);
         if(intent != null){
             animStart(intent);
         }else{
@@ -90,7 +94,7 @@ public class HomePersonalFragment extends MTFBaseFragment {
 
     @OnClick(R.id.personal_upload_book_layout)
     public void onclickUploadBook(View view){
-        Intent intent = new Intent(getActivity(),MyUploadActivity.class);
+        Intent intent = new Intent(getActivity(),MyUploadListActivity.class);
         if(intent != null){
             animStart(intent);
         }else{
@@ -101,6 +105,16 @@ public class HomePersonalFragment extends MTFBaseFragment {
     @OnClick(R.id.personal_works_management_layout)
     public void setOnclickWorksManagement(View view){
         Intent intent = new Intent(getActivity(),WorksmanagementFragmentActivity.class);
+        if(intent != null){
+            startActivityForResult(intent, 0);
+        }else{
+            Log.i("AAAA","intent==null");
+        }
+    }
+
+    @OnClick(R.id.personal_message_center_layout)
+    public void setOnclickMessageCenter(View view){
+        Intent intent = new Intent(getActivity(),MessageListActivity.class);
         if(intent != null){
             startActivityForResult(intent, 0);
         }else{
