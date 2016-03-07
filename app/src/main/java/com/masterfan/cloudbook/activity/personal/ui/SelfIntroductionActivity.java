916ce16037ee9718c4ae91e0355c6bfb56ae90ae
fgq <343887511@qq.com>
2016-03-07 14:40:36 +0800

    add progressbtn to lib & sample

--- app/src/main/java/com/masterfan/cloudbook/activity/personal/ui/SelfIntroductionActivity.java
+++ app/src/main/java/com/masterfan/cloudbook/activity/personal/ui/SelfIntroductionActivity.java
@@ -1,10 +1,18 @@
 package com.masterfan.cloudbook.activity.personal.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 
 import com.masterfan.cloudbook.R;
 import com.masterfan.library.ui.MTFBaseActivity;
 import com.masterfan.library.ui.annotation.MTFActivityFeature;
+import com.masterfan.library.widget.listview.MTFProgress;
+import com.masterfan.library.widget.probutton.MTFProcessButton;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * 自我介绍
@@ -12,6 +20,54 @@ import com.masterfan.library.ui.annotation.MTFActivityFeature;
  */
 @MTFActivityFeature(layout = R.layout.activity_self_introduction, status_bar_color = R.color.colorPrimary)
 public class SelfIntroductionActivity extends MTFBaseActivity {
+
+    @Bind(R.id.progress_btn)
+    MTFProcessButton btn;
+
+    private int count = 0;
+
+    @OnClick(R.id.progress_btn)
+    public void click(View view) {
+
+        new Thread(){
+            @Override
+            public void run() {
+                while (count < 100) {
+                    count += 8;
+                    if(count >= 100)
+                        count = 100;
+                    Message msg = handler.obtainMessage(1, count, 0);
+                    msg.sendToTarget();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage(0, count, 0);
+                msg.sendToTarget();
+            }
+        }.start();
+        btn.setProgress(50);
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    btn.setProgress(msg.arg1);
+                    btn.setText("下载中    "+ msg.arg1 +"%");
+                    break;
+                case 0:
+                    btn.setText("下载完成");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     @Override
     public void initialize(Bundle savedInstanceState) {
 
